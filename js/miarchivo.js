c2388cdf129a20150ab79cf26f155a3361417115
felipequirogg <inigofelipe@gmail.com>
2022-08-18 23:30:15 -0300

    listo para la entrega de ciclos

---
+++ js/miarchivo.js
@@ -0,0 +1,9 @@
+var i = 1;
+while (i <= 10) {
+if(i === 11){
+    console.log("Buenas buenas vas saludando 10 veces")
+}else{
+    console.log("Buenas buenas vas saludando " + i + " veces");
+i++;
+}
+}
