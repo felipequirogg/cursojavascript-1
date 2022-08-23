488e2d149dec77512e7e548e781f95219610b1b3
felipequirogg <inigofelipe@gmail.com>
2022-08-23 18:08:35 -0300

    listo simulador interactivo

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,9 +1,23 @@
-var i = 1;
-while (i <= 10) {
-if(i === 11){
-    console.log("Buenas buenas vas saludando 10 veces")
+const number = prompt('Diganos ¿cuantos usuarios va a agregar hoy?')
+
+function Persona (nombre, edad, peso, altura){
+    this.nombre = nombre,
+    this.edad = edad,
+    this.peso = peso,
+    this.altura = altura
+}
+
+for (i = 1 ; i <= number ; i++){
+    let nombre = prompt('¿Cual es su nombre?')
+    let edad = prompt('¿Que edad tiene?')
+    let peso = prompt('¿Cual es su peso?')
+    let altura = prompt('¿Cual es su altura?')
+if (nombre && edad && peso && altura){
+
+    let users = new Persona(nombre,edad,peso,altura)
+    console.log(users)
 }else{
-    console.log("Buenas buenas vas saludando " + i + " veces");
-i++;
+    alert ('Es necesario que coloque todos los datos')
 }
 }
+alert ('Perfecto, usuario inscripto en nuestra aplicacion')
